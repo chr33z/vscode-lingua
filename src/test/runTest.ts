329c1f607913d9693d719455077c951ddb908b87
Christopher Gebhardt <c.gebhardt@it-labs.de>
2020-04-10 10:32:51 +0200

    test: lingua settings test

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,22 +3,23 @@ import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
-	// const testWorkspace = path.resolve(__dirname, '../../../alberta.mobile');
-	// const testWorkspace = 'C:\\Development\\alberta.mobile';
-
+	
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
+		
 		// The path to test runner
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
+		// Testproject: https://github.com/chr33z/vscode-lingua-testproject
+		const testWorkspace = path.resolve(__dirname, '../../../vscode-lingua-testproject');
+
 		await runTests({ 
 			extensionDevelopmentPath,
 			extensionTestsPath,
-			// launchArgs: [testWorkspace]
+			launchArgs: [testWorkspace]
 		});
 	} catch (err) {
 		console.error('Failed to run tests');
--- src/test/suite/lingua-settings.test.ts
+++ src/test/suite/lingua-settings.test.ts
@@ -3,19 +3,26 @@ import * as vscode from 'vscode';
 import { LinguaSettings, readSettings, writeSettings } from '../../lingua-settings';
 
 suite('Lingua Settings', () => {
+    const deleteLinguaSettings = async function () {
+        const linguaSettingsFile = await vscode.workspace.findFiles('**/**/.lingua', `**/node_modules/**`, 1);
+        if (linguaSettingsFile.length > 0) {
+            vscode.workspace.fs.delete(linguaSettingsFile[0]);
+        }
+    };
 
-	test('read settings', async () => {
+    test('read settings', async () => {
+        deleteLinguaSettings();
         const settings: LinguaSettings = await readSettings();
-		expect(settings).to.exist;
+        expect(settings).to.exist;
     });
 
     test('write settings', async () => {
+        deleteLinguaSettings();
         let settings: LinguaSettings = await readSettings();
-        writeSettings(settings, 'translationFiles', [{ lang:'test', uri: vscode.Uri.file('') }]);
+        writeSettings(settings, 'translationFiles', [{ lang: 'test', uri: vscode.Uri.file('') }]);
         settings = await readSettings();
 
-		expect(settings.translationFiles.length).to.eq(1);
-		expect(settings.translationFiles[0].lang).to.eq('test');
+        expect(settings.translationFiles.length).to.eq(1);
+        expect(settings.translationFiles[0].lang).to.eq('test');
     });
-    
 });
