82e5f98888873e7ab0b92334e531f710c6832e4f
chris_g <chr33z@gmail.com>
2020-05-29 09:28:27 +0200

    refactor: Moved configuration settings to own class

--- src/configuration-settings.ts
+++ src/configuration-settings.ts
@@ -1,5 +1,23 @@
 import { workspace } from 'vscode';
 
-export function useFlatTranslationKeys(): boolean {
-    return workspace.getConfiguration('lingua').get<boolean>('flatTranslationKeys', false);
+export class Configuration {
+    public static useFlatTranslationKeys(): boolean {
+        return workspace.getConfiguration('lingua').get<boolean>('flatTranslationKeys', false);
+    }
+
+    public static maxTranslationLength(defaultLength: number = 80): number {
+        return workspace.getConfiguration('lingua').get<number>('decoration.maxTranslationLength') || defaultLength;
+    }
+
+    public static showInlineTranslation(defaultShow: boolean = true): boolean {
+        return workspace.getConfiguration('lingua').get<boolean>('decoration.showInlineTranslation') || defaultShow;
+    }
+
+    public static defaultLanguage(): string | undefined {
+        return workspace.getConfiguration('lingua').get<string>('defaultLanguage');
+    }
+
+    public static analysisExtension(): string {
+        return workspace.getConfiguration('lingua').get<string>('analysisExtensions') || '';
+    }
 }
