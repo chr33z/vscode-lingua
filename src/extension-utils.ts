337ef57db7bbdb2ad8c78dd361b505ac0250441c
Christopher Gebhardt <chr33z@gmail.com>
2023-08-06 09:02:11 +0200

    Lingua now also works on nx workspaces as a global configuration (#83)
    Before enabling the extension it checks wether there is a nx-json file in the workspace

--- src/extension-utils.ts
+++ src/extension-utils.ts
@@ -7,15 +7,19 @@ export function findFilesWithExtension(includeExt: string[]): Thenable<Uri[]> {
 }
 
 /**
- * Check if the current project is a angular project with ngx-translate module
+ * Check if the current workspace is a angular workspace or an NX workspace with an installed ngx-translate module
  */
 export async function isNgxTranslateProject(): Promise<boolean> {
-    const isAngular = await workspace.findFiles('**/**/angular.json', `**/node_modules/**`, 1);
-    const hasNgxTranslateModule = await workspace.findFiles('**/node_modules/**/*ngx-translate*', null, 1);
-    return isAngular.length > 0 && hasNgxTranslateModule.length > 0;
+    const isAngularWorkspace = (await workspace.findFiles('**/**/angular.json', `**/node_modules/**`, 1))?.length > 0;
+    const isNxWorkspace = (await workspace.findFiles('**/**/nx.json', `**/node_modules/**`, 1))?.length > 0;
+    const hasNgxTranslateModule = (await workspace.findFiles('**/node_modules/**/*ngx-translate*', null, 1))?.length > 0;
+    return (isAngularWorkspace || isNxWorkspace) && hasNgxTranslateModule;
 }
 
 export function setExtensionEnabled(enabled: boolean) {
     // https://github.com/Microsoft/vscode/issues/10401#issuecomment-280090759
     commands.executeCommand('setContext', 'lingua:enabled', enabled);
+    if(enabled) {
+        console.log("Enabled lingua extension on workspace");
+    }
 }
